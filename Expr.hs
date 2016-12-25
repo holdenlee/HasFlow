7f9921d0e9334b756b8d4c86855b42e62d4faf25
Holden Lee <holdenl@princeton.edu>
2016-12-24 19:18:04 -0500

    polynomials

--- Expr.hs
+++ Expr.hs
@@ -11,12 +11,13 @@
  -XFlexibleContexts
  -XDeriveFunctor
  -XUndecidableInstances
+ -XViewPatterns
 #-}
 
 module Expr where
 
 import Prelude hiding ((+), (*), (-))
-import Algebra.Additive as Additive
+import Algebra.Additive as Additive hiding (sum)
 import Algebra.Ring hiding (product)
 import Control.Monad
 import Control.Monad.Free
@@ -36,7 +37,58 @@ import MonadUtilities
 -- = TYPES
 -- == Expressions (for dimensions)
 
-data Expr = EInt Integer | ERef String | EAdd Expr Expr | EMul Expr Expr | EAdds [Expr] | EMuls [Expr] deriving Eq
+mapReduce :: (Ord b) => (a -> (b,c)) -> (b -> [c] -> [d]) -> [a] -> [d]
+mapReduce mf rf = concat . M.elems . M.mapWithKey rf . foldl 
+                  (\mp a -> 
+                            let 
+                                (b,c) = mf a
+                            in
+                              M.alter (\case 
+                                        Nothing -> Just [c]
+                                        Just x -> Just (c:x)) b mp) M.empty
+
+data Monomial = Monomial Integer [String] deriving Eq
+
+--compare the terms, ignore the coefficient
+instance Ord Monomial where
+    compare (Monomial _ x) (Monomial _ y) = compare x y
+
+monoMult :: Monomial -> Monomial -> Monomial 
+monoMult (Monomial c1 x1) (Monomial c2 x2) = Monomial (c1*c2) (sort (x1++x2))
+
+newtype Polynomial = Polynomial [Monomial]
+
+unpoly :: Polynomial -> [Monomial]
+unpoly (Polynomial li) = li
+
+pmap f = Polynomial . map f . unpoly
+
+simplifyPoly :: [Monomial] -> [Monomial] 
+simplifyPoly = mapReduce (\(Monomial i x) -> (x, i)) (\x is -> let isum = sum is in if isum==0 then [] else [Monomial isum x])
+
+pref :: String -> Polynomial
+pref s = Polynomial [Monomial 1 [s]]
+
+instance Show Monomial where
+    show (Monomial c li) = 
+        (if c==1 then "" else (show c)++"*")++(intercalate "*" li)
+
+instance Show Polynomial where
+    show (Polynomial li) = 
+        case li of
+          [] -> "0"
+          _ -> intercalate "+" $ map show li
+
+instance Additive.C Polynomial where
+    zero = Polynomial []
+    li1 + li2 = Polynomial $ simplifyPoly ((unpoly li1) ++ (unpoly li2))
+    negate = pmap (\(Monomial c x) -> Monomial (-c) x)
+
+instance Algebra.Ring.C Polynomial where
+    li1 * li2 = Polynomial $ simplifyPoly $ monoMult <$> (unpoly li1) <*> (unpoly li2)
+    fromInteger c = Polynomial $ if c==0 then [] else [Monomial c []]
+
+data Expr = EInt Integer | ERef String | EAdd Expr Expr | EMul Expr Expr | EAdds [Expr] | EMuls [Expr] deriving (Eq, Ord)
 
 instance Show Expr where
     show = \case
@@ -67,6 +119,13 @@ emulsToList e = case e of
                   EMuls es -> es
                   _ -> [e]
 
+prepForSeq :: Expr -> [[Expr]]
+prepForSeq e = case e of
+                 EMuls es -> map (\x -> [x]) es
+                 EAdds es -> [es]
+                 EInt i -> [[EInt i]]
+                 ERef a -> [[ERef a]]
+
 simplifyMonomial :: [Expr] -> [Expr]
 simplifyMonomial li = 
     let
@@ -76,9 +135,13 @@ simplifyMonomial li =
         li'' = mapMaybe (\case
                           EInt n -> Just n
                           _ -> Nothing) li
+        coeff = product li''
+        term = map ERef (sort li')
     in
-      (EInt (product li'')):(map ERef (sort li'))
-                       
+      if coeff==1 then term else (EInt coeff):term 
+
+freqTable :: (Ord a) => [a] -> [(a, Int)]
+freqTable li = map (\li -> (li!!0, length li)) $ group $ sort li
 
 normExpr :: Expr -> Expr
 normExpr e = case e of
@@ -87,15 +150,36 @@ normExpr e = case e of
                EAdd e1 e2 -> normExpr $ EAdds [e1,e2]
                EMul e1 e2 -> normExpr $ EMuls [e1,e2]
                EAdds es -> 
-                    let es' = map normExpr es
-                    in EAdds $ concat $ map eaddsToList es
-                    --TODO: need to combine like terms!!!
+                    let 
+                        es' = map normExpr es
+                    in normMonomialSum es'
                EMuls es -> 
                    let es' = map normExpr es
-                   in EAdds $ map (EMuls . simplifyMonomial) $ sequence $ map eaddsToList es
-                    --simplify products
+                   in EMuls es'
+--                   in EAdds $ map (EMuls . (concat . map emulsToList)) $ sequence $ (es' >>= prepForSeq)
+--normMonomialSum $
+--simplifyMonomial . 
+                   --simplify products
 
+normMonomialSum :: [Expr] -> Expr
+normMonomialSum es = 
+    let li = map (\(t,c) -> scalarMultMonomial c t) $ freqTable $ concat $ map eaddsToList es
+    in if (length li) == 1 then li!!0 else EAdds li
 --put this in HasFlow.Tensor
+
+scalarMultMonomial :: Int -> Expr -> Expr
+scalarMultMonomial (toInteger -> c) t = case t of
+                           EMuls ((EInt k):rest) -> 
+                               let pr = c*k
+                               in if pr==1 then removeRedundant (EMuls rest) else EMuls ((EInt pr):rest)
+                           EMuls li -> 
+                               if c==1 then removeRedundant (EMuls li) else EMuls ((EInt c):li)
+                           other -> (EInt 999)*other --shouldn't be in this case
+
+removeRedundant :: Expr -> Expr
+removeRedundant ex = case ex of
+                        EMuls [x] -> x
+                        _ -> ex
 
 -- |
 -- == Shape
@@ -144,3 +228,13 @@ instance Shapable () where
 
 instance Shapable Shape where
     toShape = id
+
+testExpr :: IO ()
+testExpr = do
+    --let a = ERef "a"
+    --let b = ERef "b"
+    --let c = ERef "c"
+    let a = pref "a"
+    let b = pref "b"
+    let c = pref "c"
+    forM_ [a*b, (a*b)*c, a*(b*c), a*a, a+a, a*b*a, a+b, a+a+b, (a+b)*(a+b), (a+b)*(a-b)] (putStrLn . show)
